7e2b66172123d94d7337d1e84b6090de61581115
Pavel Tvaladze <github@link-mail.cf>
2016-01-02 19:46:22 +0100

    small fixes and redesigned error message

--- SpeakUp/Properties/AssemblyInfo.cs
+++ SpeakUp/Properties/AssemblyInfo.cs
@@ -2,11 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется с помощью 
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
 [assembly: AssemblyTitle("SpeakUP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Peer-2-peer videoconferences")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LinkSoft")]
 [assembly: AssemblyProduct("SpeakUP")]
@@ -14,23 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
-// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("fc02dcd9-e6d7-437f-b387-ff0af6e5081e")]
 
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии 
-//      Номер построения
-//      Редакция
-//
-// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
